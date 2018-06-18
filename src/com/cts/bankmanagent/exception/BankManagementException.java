be30fd730a06065a1a9443d01a267de347220c67
tejashree9 <tejashreesdhope@rediffmail.com>
2018-06-18 17:45:33 +0530

    Add files via upload

---
+++ src/com/cts/bankmanagent/exception/BankManagementException.java
@@ -0,0 +1,23 @@
+package com.cts.bankmanagement.exception;
+
+public class BankManagementException extends Exception {
+
+	public BankManagementException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public BankManagementException(String arg0) {
+		super(arg0);
+	}
+
+	public BankManagementException(Throwable arg0) {
+		super(arg0);
+
+	}
+
+	public BankManagementException() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+}
